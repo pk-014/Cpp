98cba39a2befc3e4c7ea13e15f3c946bc304d664
Muhammad Asad <wolverine1786@gmail.com>
2023-03-16 23:17:21 +0500

    Some More Files added

--- Patterns/Alphabet_Triangle.cpp
+++ Patterns/Alphabet_Triangle.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <unistd.h>
+
 using namespace std;
 int main()
 {
@@ -7,20 +9,25 @@ int main()
 
 do
 {
+	
     system("Color 70");
 	cout<<"\n\t=>Programming Fundamentals"<<endl;
 	cout<<"\t=>Presented By: Muhammad Asad"<<endl;
 	cout<<"\t=>Roll No. : BSF2201587"<<endl<<endl;
-    int a=1;
+    int a=1,rows;
     char alphabet='z';
+    cout<<"Enter Number Of Rows: ";cin>>rows;
     // Creating Rows=4
-    for (int i = 1; i <=4; i++)
+    
+    for (int i = 1; i <=rows; i++)
     {
+    	cout<<"\t\t\t\t\t";
 
 
         // Spaces
-        for (int j = 3; j >=i; j--)
+        for (int j = rows-1; j >=i; j--)
         {
+			usleep(300000);
             std::cout<<"  ";
         }
 
@@ -32,10 +39,11 @@ do
         
         for (int k = 1; k <= a; k++)
         {
+        	usleep(300000);
             std::cout<<temp_alphabet++<<" ";
         }
         
-        for (int l = i*2; l <= 7; l++)
+        for (int l = i*2; l <= 9; l++)
         {
             if (l%2!=0)
             {
@@ -46,8 +54,14 @@ do
         a=a+2;       
         std::cout<<endl;
     }
-std::cout<<"\nWant to Re-Run the Program?\n\f\'y\' for YES \t\t \'n\' for NO\n";
+cout<<endl<<"\t\t\t";
+for(int m=1;m<=45;m++){
+	cout<<"=";
+}
+cout<<endl;
+std::cout<<"\n\t\t\t\tWant to Run Program again?\n\t\t\t\t=>\'y\' for YES \t =>\'n\' for NO\n\t\t\t\t\t";
 std::cin>>rerun;
+
 system("CLS");
 }while (rerun=='y' || rerun=='Y');
 
